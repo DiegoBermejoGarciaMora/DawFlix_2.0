2ab2a00d46cfe9042b1762d603dba1fadf2698fd
Diego <151567790+DiegoBermejoGarciaMora@users.noreply.github.com>
2025-05-22 17:18:22 +0200

    Delete DawFlix_2.0 directory

--- DawFlix_2.0/DawFlix/src/Model/Contenido/Generos/GeneroMusica.java
+++
@@ -1,5 +0,0 @@
-package Model.Contenido.Generos;
-
-public enum GeneroMusica {
-    POP, ROCK, HIP_HOP, JAZZ, CLASICA, ELECTRONICA, REGGAETON, METAL, INDIE, FOLK, COUNTRY, BLUES, BALADA, SALSA
-}
--- DawFlix_2.0/DawFlix_2.0/DawFlix_2.0/DawFlix/src/Interface/Reproducible.java
+++
@@ -1,5 +0,0 @@
-package Interface;
-
-public interface Reproducible {
-    void reproducir();
-}
